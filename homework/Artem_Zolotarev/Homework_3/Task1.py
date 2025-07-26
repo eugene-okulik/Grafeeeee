f56df7e437d1cd5afe12956656123b7b0cf62081
Grafeeeeee <workingmailgrafee@mail.ru>
2025-07-26 14:06:07 +0300

    Fixed some errors to tasks of Homework_3

--- homework/Artem_Zolotarev/Homework_3/Task1.py
+++ homework/Artem_Zolotarev/Homework_3/Task1.py
@@ -3,4 +3,4 @@ import random
 a = random.randint(1, 100)
 b = random.randint(1, 100)
 print(a, b)
-print((a+b), (a-b), a*b)
+print((a + b), (a - b), a * b)
--- homework/Artem_Zolotarev/Homework_3/Task4.py
+++ homework/Artem_Zolotarev/Homework_3/Task4.py
@@ -4,8 +4,8 @@ a = random.randint(1, 10)
 b = random.randint(1, 10)
 print(a, b)
 
-c = (a**2+b**2)**(1/2)
+c = (a ** 2+b ** 2) ** (1/2)
 print(c)
 
-square_triangle = 0.5*a*b
+square_triangle = 0.5 * a * b
 print(square_triangle)
