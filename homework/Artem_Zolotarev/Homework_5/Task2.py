0c7d283330735843c27391b8bb9d6dd21e0f19b8
Grafeeeeee <workingmailgrafee@mail.ru>
2025-07-27 12:02:08 +0300

    Fixed errors by Flake8

--- homework/Artem_Zolotarev/Homework_5/Task2.py
+++ homework/Artem_Zolotarev/Homework_5/Task2.py
@@ -7,10 +7,11 @@ c = 'результат работы программы: 9'
 
 def find_index(string, text):
     try:
-        return text.index(string)+2
+        return text.index(string) + 2
     except ValueError:
         return -1
 
-print(int(a[find_index(': ', a):])+10)
-print(int(b[find_index(': ', b):])+10)
-print(int(c[find_index(': ', c):])+10)
+
+print(int(a[find_index(': ', a):]) + 10)
+print(int(b[find_index(': ', b):]) + 10)
+print(int(c[find_index(': ', c):]) + 10)
