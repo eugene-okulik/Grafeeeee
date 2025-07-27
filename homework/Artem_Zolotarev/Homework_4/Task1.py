6ac98145217852be59e5db08cc01c197cb8d4e2e
Grafeeeeee <workingmailgrafee@mail.ru>
2025-07-27 12:40:16 +0300

    Edited naming key 'I am a tuple'

--- homework/Artem_Zolotarev/Homework_4/Task1.py
+++ homework/Artem_Zolotarev/Homework_4/Task1.py
@@ -5,7 +5,7 @@ my_dict = {"tuple": (1, 2, 3, 4, 5), "list": [1, 2, 3, 4, 5],
 print(my_dict["tuple"][-1])
 my_dict["list"].append(6)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = (1,)
+my_dict['dict'][('i am a tuple',)] = (1,)
 del my_dict['dict'][2]
 my_dict['set'].add(17)
 my_dict['set'].remove(17)
