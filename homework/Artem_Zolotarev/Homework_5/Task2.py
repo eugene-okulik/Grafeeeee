fdaedc2f7495acf77a0493c3ca49de70b53ee1c0
Grafeeeeee <workingmailgrafee@mail.ru>
2025-07-27 11:58:17 +0300

    Fixed some errors to tasks

--- homework/Artem_Zolotarev/Homework_5/Task2.py
+++ homework/Artem_Zolotarev/Homework_5/Task2.py
@@ -4,6 +4,13 @@ b = 'результат операции: 514'
 
 c = 'результат работы программы: 9'
 
-print(10 + int(a[20:]))
-print(10 + int(b[20:]))
-print(10 + int(c[28:]))
+
+def find_index(string, text):
+    try:
+        return text.index(string)+2
+    except ValueError:
+        return -1
+
+print(int(a[find_index(': ', a):])+10)
+print(int(b[find_index(': ', b):])+10)
+print(int(c[find_index(': ', c):])+10)
--- homework/Artem_Zolotarev/Homework_5/Task3.py
+++ homework/Artem_Zolotarev/Homework_5/Task3.py
@@ -1,4 +1,5 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
+joined_students = ', '.join(students)
 subjects = ['math', 'biology', 'geography']
-print(f'Students {students[0]}, {students[1]}, {students[2]} study these subjects: '
-      f'{subjects[0]}, {subjects[1]}, {subjects[2]}')
+joined_subjects = ', '.join(subjects)
+print(f'Students {joined_students} study these subjects: {joined_subjects}')
