2f35b168e6eba778a3d7a6faf4cf538ffde41f25
Grafeeeeee <workingmailgrafee@mail.ru>
2025-07-31 15:11:28 +0300

    Edited task2 from HW_8

--- homework/Artem_Zolotarev/Homework_8/Task2.py
+++ homework/Artem_Zolotarev/Homework_8/Task2.py
@@ -1,15 +1,26 @@
 from sys import set_int_max_str_digits
 
 
-def fibonnachi(x):
+def fibonacci(limit):
     a, b = 0, 1
-    for var in range(x):
+    counter = 0
+    while counter < limit:
         a, b = b, a + b
-    return a
+        yield a
+        counter += 1
 
 
-print(fibonnachi(5))
-print(fibonnachi(200))
-print(fibonnachi(1000))
-set_int_max_str_digits(100000)
-print(fibonnachi(100000))
+def find_fibonacci(num):
+    counter = 1
+    number = 0
+    for x in fibonacci(10000000):
+        if counter == num:
+            number = x
+            break
+        counter += 1
+    return number
+print(find_fibonacci(5))
+print(find_fibonacci(200))
+print(find_fibonacci(1000))
+set_int_max_str_digits(10000000)
+print(find_fibonacci(100000))
