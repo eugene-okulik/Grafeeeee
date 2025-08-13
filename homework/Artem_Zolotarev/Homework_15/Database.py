2b16ed240ab980b88da7c987a53eeaf3991f1893
Grafeeeeee <workingmailgrafee@mail.ru>
2025-08-14 01:25:36 +0300

    Edited fields by id HM_15

--- homework/Artem_Zolotarev/Homework_15/Database.py
+++ homework/Artem_Zolotarev/Homework_15/Database.py
@@ -9,43 +9,49 @@ db = mysql.connect(
     database='st-onl'
 )
 cursor = db.cursor()
-cursor.execute("INSERT INTO students (name, second_name) VALUES (%s, %s)", ('artem2', 'zolotarev2'))
+cursor.execute("INSERT INTO students (name, second_name) VALUES ('artem2', 'zolotarev2')")
+student_id = cursor.lastrowid
 
-cursor.execute("INSERT INTO books (title, taken_by_student_id) "
-               "VALUES (%s, %s)", ('Basic python', 20963))
+cursor.execute(f"INSERT INTO books (title, taken_by_student_id) VALUES ('Basic python', {student_id})")
 
-cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES (%s, %s, %s)",
-               ('Typical Python', 'Aug2025', 'Jan2026'))
+cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES ('Typical Python', 'Aug2025', 'Jan2026')")
+group_id = cursor.lastrowid
+cursor.execute(f"UPDATE students s SET s.group_id = {group_id} WHERE s.id = {student_id})")
 
-cursor.execute("UPDATE students s SET s.group_id = %s WHERE s.id = 20963", (5521,))
+cursor.execute("INSERT INTO subjects (title) VALUES ('OOP')")
+subject1_id = cursor.lastrowid
+cursor.execute("INSERT INTO subjects (title) VALUES ('Manual testing')")
+subject2_id = cursor.lastrowid
+cursor.execute("INSERT INTO subjects (title) VALUES ('Auto_testing')")
+subject3_id = cursor.lastrowid
 
-cursor.execute("INSERT INTO subjects (title) VALUES (%s), (%s), (%s) ",
-               ('OOP', 'Manual testing', 'Auto_testing'))
+cursor.execute(f"INSERT INTO lessons (title, subject1_id) VALUES ('theory', {subject1_id})")
+lesson1_id = cursor.lastrowid
+cursor.execute(f"INSERT INTO lessons (title, subject_id) VALUES ('practice', {subject1_id})")
+lesson2_id = cursor.lastrowid
+cursor.execute(f"fINSERT INTO lessons (title, subject_id) VALUES ('test-case', {subject2_id})")
+lesson3_id = cursor.lastrowid
+cursor.execute(f"INSERT INTO lessons (title, subject_id) VALUES ('bag-repots', {subject2_id})")
+lesson4_id = cursor.lastrowid
+cursor.execute(f"INSERT INTO lessons (title, subject_id) VALUES ('kibana', {subject3_id})")
+lesson5_id = cursor.lastrowid
+cursor.execute(f"INSERT INTO lessons (title, subject_id) VALUES ('grafana', {subject3_id})")
+lesson6_id = cursor.lastrowid
 
-cursor.execute("INSERT INTO lessons (title, subject_id) "
-               "VALUES (%s, %s), (%s, %s)", ('theory', 11673, 'practice', 11673))
-
-cursor.execute("INSERT INTO lessons (title, subject_id) "
-               "VALUES (%s, %s), (%s, %s)", ('test-case', 11674, 'bag-repots', 11674))
-
-cursor.execute("INSERT INTO lessons (title, subject_id) "
-               "VALUES (%s, %s), (%s, %s)", ('kibana', 11675, 'grafana', 11675))
-
-lst_values = [(5, 11775, 20963),
-              (4, 11774, 20963),
-              (3, 11773, 20963),
-              (2, 11772, 20963),
-              (2, 11771, 20963),
-              (3, 11770, 20963)
+lst_values = [(5, lesson6_id, student_id),
+              (4, lesson5_id, student_id),
+              (3, lesson4_id, student_id),
+              (2, lesson3_id, student_id),
+              (2, lesson2_id, student_id),
+              (3, lesson1_id, student_id)
               ]
 cursor.executemany("INSERT INTO marks (value, lesson_id, student_id) VALUES (%s, %s, %s)", lst_values)
-db.commit()
 
-cursor.execute("SELECT marks.value FROM marks WHERE marks.student_id = 20963")
+cursor.execute(f"SELECT marks.value FROM marks WHERE marks.student_id = {student_id}")
 data = cursor.fetchall()
 print(data)
 
-cursor.execute("SELECT title FROM books WHERE books.taken_by_student_id = 20963")
+cursor.execute(f"SELECT title FROM books WHERE books.taken_by_student_id = {student_id}")
 data = cursor.fetchall()
 print(data)
 
@@ -57,7 +63,7 @@ cursor.execute(
     "JOIN marks m ON m.student_id = s.id "
     "JOIN lessons l ON l.id = m.lesson_id "
     "JOIN subjects s2 ON s2.id = l.subject_id "
-    "WHERE s.id = 20963 "
+    f"WHERE s.id = {student_id} "
 )
 data = cursor.fetchall()
 for line in data:
